111c12bc072a90a72e9b2921fe530a2e30ec30cc
Christopher Marsh <christopher@ascensusglobal.com>
2025-02-10 22:04:54 -0800

    add authz dynamic encoding

--- infra/lib/handlers/viewerRequestHandler.js
+++ infra/lib/handlers/viewerRequestHandler.js
@@ -5,6 +5,7 @@
 ////////////////////////////////////////////////////////////////////////
 function handler(event) {
   metadataRewriteHandler(event);
+  volunteersRewriteHandler(event);
   let requestOrResponse = authzHandler(event);
   return requestOrResponse;
 }
@@ -17,7 +18,7 @@ function handler(event) {
 function authzHandler(event) {
   let authzHeaders = event.request.headers.authorization;
   let requestOrResponse = event.request;
-  let expectedCreds = 'Basic Y2hyaXN0b3BoZXI6YmluZ28h';
+  let expectedCreds = `Basic ${btoa('kim:pewpew')}`;
 
   let prod = production(event.request.headers);
   let authorized = authzHeaders && authzHeaders.value === expectedCreds;
@@ -41,6 +42,21 @@ function metadataRewriteHandler(event) {
   let regex = /metadata\/?$/i;
 
   uri = uri.replace(regex, 'metadata.json');
+  request.uri = uri;
+  return request;
+}
+
+////////////////////////////////////////////////////////////////////////
+//
+// Rewrites requests for /metadata to /metadata.json
+//
+////////////////////////////////////////////////////////////////////////
+function volunteersRewriteHandler(event) {
+  let request = event.request;
+  let uri = request.uri;
+  let regex = /volunteers\/?$/i;
+
+  uri = uri.replace(regex, 'volunteers.json');
   request.uri = uri;
   return request;
 }
